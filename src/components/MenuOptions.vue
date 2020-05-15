d01f9976f1d4a1a77401c2bc30e1955c3db2ca01
Victor <vcs5743@gmail.com>
2020-05-15 01:12:41 -0400

    Sidebar finished

--- src/components/MenuOptions.vue
+++ src/components/MenuOptions.vue
@@ -1,7 +1,7 @@
 <template>
-	<a class="flex items-center ml-4 mt-4" href="#">
+	<a class="flex items-center ml-4" href="#">
 		<span v-html="options.svg"></span>
-		<span class="ml-2 mt-1 text-gray-400 hover:text-white">{{ options.title }}</span>
+		<span class="ml-2 mt-1 text-gray-500 hover:text-white">{{ options.title }}</span>
 	</a>
 </template>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,28 +1,41 @@
 <template>
 	<div class="w-1/5 flex flex-none flex-col justify-between bg-first">
+		<!-- Menu Options -->
 		<div>
 			<ul class="py-2">
-				<li
-					class="border-l-4 border-green-600"
-					v-for="(item, index) in options"
-					:key="item.id"
-				>
+				<li class="mt-4" v-for="(item, index) in options" :key="item.id">
 					<MenuOptions :options="options[index]" />
 				</li>
 			</ul>
 		</div>
+
+		<!-- Music Options -->
 		<div class="overflow-y-auto">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ducimus accusamus
-			deleniti consectetur sit facilis sequi, dignissimos recusandae eligendi minima
-			blanditiis animi tempora minus veritatis voluptates ad similique earum. Repellendus?
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ducimus accusamus
-			deleniti consectetur sit facilis sequi, dignissimos recusandae eligendi minima
-			blanditiis animi tempora minus veritatis voluptates ad similique earum. Repellendus?
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ducimus accusamus
-			deleniti consectetur sit facilis sequi, dignissimos recusandae eligendi minima
-			blanditiis animi tempora minus veritatis voluptates ad similique earum. Repellendus?
+			<!-- Library -->
+			<div class="px-4 mt-2">
+				<h3 class="uppercase tracking-widest text-gray-500 text-xs">Your Library</h3>
+				<ul class="leading-loose">
+					<li v-for="item in library" :key="item.id">
+						<a class="hover:text-white" href="#">{{ item.msg }}</a>
+					</li>
+				</ul>
+			</div>
+
+			<!-- Playlists -->
+			<div class="px-4 mt-4">
+				<h3 class="uppercase tracking-widest text-gray-500 text-xs">Playlists</h3>
+				<ul class="leading-loose">
+					<li class="truncate" v-for="item in playlists" :key="item.id">
+						<a class="hover:text-white" href="#">{{ item.msg }}</a>
+					</li>
+				</ul>
+			</div>
 		</div>
-		<div>Three</div>
+
+		<!-- New Playlist Option -->
+		<div class="py-2 border-t border-gray-500">
+			<MenuOptions :options="playlist" />
+		</div>
 	</div>
 </template>
 
@@ -50,6 +63,38 @@ export default {
 						'<svg class="fill-current text-white" width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M20 9v9a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h13.8L.74 1.97 1.26.03 20 5.06V9zm-5 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM2 8v2h16V8H2zm1.5 10a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6.5-1a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/></svg>',
 				},
 			],
+			library: [
+				{ msg: 'Made For You' },
+				{ msg: 'Recently Played' },
+				{ msg: 'Liked Songs' },
+				{ msg: 'Albums' },
+				{ msg: 'Artists' },
+				{ msg: 'Podcasts' },
+			],
+			playlists: [
+				{ msg: 'Acoustic Hits' },
+				{ msg: 'Acoustic Covers' },
+				{ msg: 'Discovery Weekly' },
+				{ msg: 'Hot Hits Canada' },
+				{ msg: "90's Hip-Hop" },
+				{ msg: "90's Smash Hits" },
+				{ msg: 'TGIF' },
+				{ msg: 'Mint Canada' },
+				{ msg: 'Hip-Hop Central' },
+				{ msg: 'R&B Right Now' },
+				{ msg: "I Love My 'OOs R&B" },
+				{ msg: "Chris Lake's Dance" },
+				{ msg: 'I Love My 90s R&B Hip-Hop' },
+				{ msg: 'Are & Be' },
+				{ msg: "Today's Top Hits" },
+				{ msg: 'Get Turnt' },
+				{ msg: 'iTunes' },
+			],
+			playlist: {
+				title: 'New playlist',
+				svg:
+					'<svg class="fill-current text-white" width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12zM7 7V5h2v2h2v2H9v2H7V9H5V7h2z"/></svg>',
+			},
 		};
 	},
 	components: {
